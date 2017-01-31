73a2139c519638f85f2b14eb813c839bafd859b8
sharonramdass <sharonramdass@Kevins-MacBook-Pro.local>
2017-01-31 19:13:30 -0400

    Student form almost complete
    Still to add Contact Information etc

--- nghs-sms/app/controllers/StudentController.java
+++ nghs-sms/app/controllers/StudentController.java
@@ -32,16 +32,29 @@ public class StudentController extends Controller {
 
         DynamicForm dynamicForm = Form.form().bindFromRequest();
         String firstName = dynamicForm.get("firstName");
+        String middleName = dynamicForm.get("middleName");
+        String lastName = dynamicForm.get("lastName");
+        String dateOfBirth = dynamicForm.get("dateOfBirth");
+        String address = dynamicForm.get("address");
 
         Http.MultipartFormData<File> body = request().body().asMultipartFormData();
-        Http.MultipartFormData.FilePart<File> picture = body.getFile("profilePicture");
-        if (picture != null) {
-            String fileName = picture.getFilename();
-            String contentType = picture.getContentType();
-            File file = picture.getFile();
+        Http.MultipartFormData.FilePart<File> picture1 = body.getFile("profilePicture");
+        Http.MultipartFormData.FilePart<File> picture2 = body.getFile("birthCertificate");
+        if (picture1 != null) {
+            String fileName = picture1.getFilename();
+            String contentType = picture1.getContentType();
+            File profilePicFile = picture1.getFile();
+            String fileName2 = picture2.getFilename();
+            String contentType2 = picture2.getContentType();
+            File file2 = picture2.getFile();
             Student newStudent = new Student();
             newStudent.firstName = firstName;
-            newStudent.profilePic = file;
+            newStudent.middleName = middleName;
+            newStudent.lastName = lastName;
+            newStudent.DOB = dateOfBirth;
+            newStudent.address = address;
+            newStudent.profilePic = profilePicFile;
+            newStudent.birthCertificate = file2;
             newStudent.save();
 //            File oldFile = new File("public/images/profilePic");
 //            oldFile.delete();
@@ -59,5 +72,10 @@ public class StudentController extends Controller {
         Student student = Student.find.byId(studentId);
         File file = student.profilePic;
         return ok(new ByteArrayInputStream(Files.readAllBytes(file.toPath()))).as("image/jpeg");
+    }
+    public Result getStudentBirthCertificate(Long studentId) throws IOException {
+        Student student = Student.find.byId(studentId);
+        File file2 = student.birthCertificate;
+        return ok(new ByteArrayInputStream(Files.readAllBytes(file2.toPath()))).as("image/jpeg");
     }
 }
